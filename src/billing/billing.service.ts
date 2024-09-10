77a4a095d846f9c706fc73100799bec1b0cd149b
devsamahd <samahd626@gmail.com>
2024-09-10 12:17:35 +0100

    ...

--- src/billing/billing.service.ts
+++ src/billing/billing.service.ts
@@ -1,120 +1,83 @@
-import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { Injectable, HttpException } from '@nestjs/common';
 import axios from 'axios';
-import { Request } from 'express';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class BillingService {
   private readonly flutterwaveUrl: string;
   private readonly flutterwaveSecret: string;
+  private readonly valuePerPoint:number;
 
-  constructor() {
+  constructor(private readonly prisma: PrismaService) {
     this.flutterwaveUrl = process.env.FLUTTERWAVE_URL;
     this.flutterwaveSecret = process.env.FLW_KEY;
+    this.valuePerPoint = 0.5
   }
   
-  async findOne(req: Request) {
+  async initializePayment(email: string, amount: number, phone_number: string, name: string, userId:string): Promise<any> {
     try {
-      const user: any = req.user;
-  
-      // Fetch subscriptions
-      const response = await axios.get(`${this.flutterwaveUrl}/subscriptions`, {
-        headers: { Authorization: `Bearer ${this.flutterwaveSecret}` },
-        params: { email: user.email },
-      });
-  
-      const subscriptions = response.data.data;
-  
-      if (!subscriptions || subscriptions.length === 0) {
-        throw new HttpException('Subscription not found', HttpStatus.NOT_FOUND);
-      }
-  
-      // Fetch plan details for each subscription
-      const enrichedSubscriptions = await Promise.all(
-        subscriptions.map(async (subscription: any) => {
-          try {
-            const planResponse = await axios.get(`${this.flutterwaveUrl}/payment-plans/${subscription.plan}`, {
-              headers: { Authorization: `Bearer ${this.flutterwaveSecret}` },
-            });
-            return {
-              ...subscription,
-              plan: planResponse.data.data, // Add plan details to the subscription object
-            };
-      
-
-          } catch (error) {
-            console.error(`Failed to fetch plan for subscription ID: ${subscription.id}`);
-            return subscription; // If plan fetch fails, return the subscription without plan details
-          }
-        })
-      );
-  
-      return enrichedSubscriptions;
-    } catch (error) {
-      throw new HttpException(error.response?.data?.message || 'Error fetching subscription', HttpStatus.NOT_FOUND);
-    }
-  }
-
-  async remove(id: number) {
-    try {
-      // Delete subscription in Flutterwave
-      const response = await axios.put(
-        `${this.flutterwaveUrl}/subscriptions/${id}/cancel`,
+      const response = await axios.post(
+        this.flutterwaveUrl+'/payments',
         {
-          headers: { Authorization: `Bearer ${this.flutterwaveSecret}` },
+          tx_ref: `omashu-${Date.now()}`,
+          amount: amount,
+          currency: 'USD',
+          redirect_url: `${process.env.BASE_URL}/payment/callback`,
+          payment_options: 'card',
+          customer: {
+            email: email,
+            phonenumber: phone_number,
+            name: name,
+          },
+          customizations: {
+            title: 'Allwebtool Points Purchase',
+            description: 'Purchase points to use on Allwebtool platform',
+            logo: 'https://cdn.allwebtool.com/allwebtool_assets/logo.png',
+          },
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${this.flutterwaveSecret}`,
+            'Content-Type': 'application/json',
+          },
         },
       );
 
-      return { message: 'Subscription deactivated successfully', data: response.data };
+      const points = amount/this.valuePerPoint
+
+      await this.prisma.transaction.create({data:{
+        userId,
+        refId: response.data.refId,
+        points,
+        amount: amount,       
+        type: "credit",
+        status: "initiated"
+      }})
+      return response.data;
     } catch (error) {
-      console.log(error)
-      throw new HttpException(error.response?.data?.message || 'Error deleting subscription', HttpStatus.BAD_REQUEST);
+      throw new HttpException(error.response.data, error.response.status);
     }
   }
 
-  async reactivate(id: number) {
+  async verifyPayment(transactionId: string, userEmail:string): Promise<any> {
     try {
-      // Delete subscription in Flutterwave
-      const response = await axios.put(
-        `${this.flutterwaveUrl}/subscriptions/${id}/activate`,
-        {
-          headers: { Authorization: `Bearer ${this.flutterwaveSecret}` },
-        },
-      );
-
-      return { message: 'Subscription deactivated successfully', data: response.data };
-    } catch (error) {
-      throw new HttpException(error.response?.data?.message || 'Error deleting subscription', HttpStatus.BAD_REQUEST);
-    }
-  }
-
-  async checkSubscriptionStatus(req: Request) {
-    try {
-      const user: any = req.user;
       const response = await axios.get(
-        `${this.flutterwaveUrl}/subscriptions`,
+        this.flutterwaveUrl+`/transactions/${transactionId}/verify`,
         {
-          headers: { Authorization: `Bearer ${this.flutterwaveSecret}` },
-          params: { email: user.email },
+          headers: {
+            Authorization: `Bearer ${this.flutterwaveSecret}`,
+            'Content-Type': 'application/json',
+          },
         },
       );
-
-      const activeSubscriptions = (response?.data?.data ?? [])
-      .filter((subscription: any) => subscription.status === 'active')
-      .sort((a: any, b: any) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime());
-
-   
-      if (activeSubscriptions.length > 0) {
-        console.log(activeSubscriptions)
-        return activeSubscriptions
-      } else {
-        return { status: 400, message: 'No active subscriptions found' };
-      }
+      const resp = response?.data?.data
+      await this.prisma.transaction.updateMany({where:{refId: transactionId}, data:{status: "successful"}})
+      await this.prisma.user.updateMany({where:{email: userEmail}, data:{cardToken: resp.card?.token, lastDigit:resp.card.last_4digits }})
+      return response.data;
     } catch (error) {
-      console.log(error.response);
-      throw new HttpException('Failed to check subscription status', HttpStatus.BAD_REQUEST);
+      await this.prisma.transaction.updateMany({where:{refId: transactionId}, data:{status: "failed"}})
+      throw new HttpException(error.response.data, error.response.status);
     }
   }
 
-
-  
 }
